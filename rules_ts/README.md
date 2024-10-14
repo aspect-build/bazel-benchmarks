59221cb322ca78013548ebe006bfefdc31220acd
Greg Magolan <greg@aspect.dev>
2024-10-13 21:56:54 -0700

    Remove old rules_ts benchmark

--- rules_ts/README.md
+++
@@ -1,5 +0,0 @@
-# npm benchmarks
-
-Run with `./run_benchmark.sh` script.
-
-Results and write-up will be published to https://blog.aspect.dev.
--- rules_ts/generate.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit -o nounset
-
-style=$1
-
-rm -rf "$style"
-node generator.js "$style" "$style" 3 10 10 1000
